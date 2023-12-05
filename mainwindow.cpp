589eb7f7deacafb97445e41014f28a4974640e6e
Maksim Arkaev <maksim.arkaev.03@mail.ru>
2023-12-05 18:10:17 +0300

    Ver 0.5 The most piece of gui and half of functional

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -10,8 +10,38 @@ selectColor->setWindowFlags(Qt::Widget);
 selectColor->setOptions(QColorDialog::DontUseNativeDialog |QColorDialog::NoButtons);
 OKButton = new QPushButton(this);
 OKButton->setText("OK");
-MainWindow::OKButton->move(405, 360);
+
+QString QTemp = selectColor->selectedColor().name();
+
+
+
+
+WinHighlightingOptionsWithoutParametrs = "Windows Registry Editor Version 5.00\n\n"
+                                         "[HKEY_CURRENT_USER\\Control Panel\\Colors]\n\"Hilight\"=\"188 188 188\"\n\n"
+                                         "[HKEY_CURRENT_USER\\Control Panel\\Colors]\n\"HotTrackingColor\"=\"117 117 117\"";
+path = "testFile.txt";
+
+QFile fmove(path);
+if (fmove.open(QIODevice::WriteOnly)){
+QTextStream out(&fmove);
+out<<WinHighlightingOptionsWithoutParametrs.toUtf8();
+fmove.close();
 }
+
+
+
+connect(OKButton, SIGNAL(clicked()), this, SLOT(QTemp>>fmove));
+
+
+
+
+
+
+MainWindow::OKButton->move(405, 360);
+
+}
+
+
 
 MainWindow::~MainWindow()
 {
--- mainwindow.h
+++ mainwindow.h
@@ -6,13 +6,22 @@
 #include <QColorDialog>
 #include <QString>
 #include <QCheckBox>
+#include <QFile>
+#include <QTextStream>
 
+#include <fstream>
+#include <iostream>
 
 class QPushButton;
 class QWidget;
 class QColorDialog;
 class QCheckBox;
 class QString;
+class QFile;
+class QTextStream;
+
+
+using namespace std;
 
 
 class MainWindow : public QMainWindow
@@ -26,11 +35,15 @@ public:
 
 private:
 
+
+string Temp;
+QString path;
+QString WinHighlightingOptionsWithoutParametrs;
 QColorDialog *selectColor;
 QPushButton *OKButton;
 QCheckBox *selectCrusor;
 QCheckBox *selectHighLighting;
-
+QString QTemp;
 
 
 
